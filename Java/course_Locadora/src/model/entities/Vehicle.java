85701bece9d500829d238881ce9a76624634c275
Arthur Abade <arthurnz.personal@gmail.com>
2024-10-14 12:26:00 -0300

    ex sem interface

---
+++ Java/course_Locadora/src/model/entities/Vehicle.java
@@ -0,0 +1,20 @@
+package course_Locadora.src.model.entities;
+
+public class Vehicle {
+
+    private String model;
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+    public Vehicle(){
+    }
+    
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    
+}
---
+++ Java/course_Locadora/src/model/services/RentalService.java
@@ -0,0 +1,35 @@
+package course_Locadora.src.model.services;
+
+import java.time.Duration;
+
+import course_Locadora.src.model.entities.CarRental;
+import course_Locadora.src.model.entities.Invoice;
+
+public class RentalService {
+
+    private Double pricePerDay;
+    private Double pricePerHour;
+
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60;
+        double basicPayment;
+        if (hours <= 12) {
+            basicPayment = pricePerHour * Math.ceil(hours);
+        }
+        else {
+            basicPayment = pricePerDay * Math.ceil(hours / 24);
+        }
+        double tax = taxService.tax(basicPayment);
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+    }
+    
+}
